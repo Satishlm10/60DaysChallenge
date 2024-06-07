10e0f88600a7c58645ceee664271b5d152123fad
satish maharjan <satish.maharjan77@gmail.com>
2024-06-07 21:29:56 +0545

    Entity Mapping Practice

---
+++ Day-07/SpringDataJpa-EntityMapping-Practice/src/test/java/com/_daysLearningChallenge/SpringDataJpa_EntityMapping_Practice/SpringDataJpaEntityMappingPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com._daysLearningChallenge.SpringDataJpa_EntityMapping_Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaEntityMappingPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
