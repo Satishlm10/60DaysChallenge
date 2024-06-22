4f7f437d567e331d52cd3a55c7db391bce09aa3f
satish maharjan <satish.maharjan77@gmail.com>
2024-06-22 15:41:06 +0545

    Day:22 authorization based on persmissions and role based authorization

---
+++ Day-21/SpringSecurityBasics/src/main/java/com/_daysLearningChallenge/SpringSecurityBasics/SecuredBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com._daysLearningChallenge.SpringSecurityBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredBankBackendApplication.class, args);
+	}
+
+}
